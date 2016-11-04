58aa88ad56ea60ed128b29ed33e74c4cddb686c2
pingul <joel@wbn.se>
2016-11-04 15:10:16 +0100

    First commit

--- SixTrack/sixtrack.s
+++ SixTrack/sixtrack.s
@@ -47401,6 +47401,28 @@ C      write(*,*) "DBGDBG c:", funName, len(funName)
      &           csets_unique_dynk(nsets_unique_dynk,2) ))
             found = .false.
 
+            if (element_name_s .eq. "GLOBAL-VARS") then
+               found=.true.
+               
+               if (.not. (att_name_s .eq. "E0") ) then
+                  badelem=.true.
+                  write(*,*) "DYNK>E0 != '", att_name_s
+               endif
+
+               !TODO: Small sanity check - We should only modify energy if in 6d tracking...
+               
+               if (badelem) then
++if cr
+                  write(lout,*) "DYNK> Insane - attribute '",
++ei
++if .not.cr
+                  write(*,*)    "DYNK> Insane - attribute '",
++ei
+     &                 att_name_s, "' is not valid for GLOBAL-VARS"
+                     call prror(-1)
+                  endif
+            endif
+            
             do jj=1,il
                if ( bez(jj).eq. element_name_s) then
                   
@@ -47482,7 +47504,7 @@ C      write(*,*) "DBGDBG c:", funName, len(funName)
             endif
 
             ! Store original value of data point
-            fsets_origvalue_dynk(nsets_unique_dynk) =  
+            fsets_origvalue_dynk(nsets_unique_dynk) =
      &           dynk_getvalue(csets_dynk(ii,1),csets_dynk(ii,2))
          endif
       enddo
@@ -48201,6 +48223,7 @@ C+ei
 +ca parnum
 +ca common
 +ca commonmn
++ca commonm1 ! ASK: Added this to include e0f. Ok?
 +ca commontr
 +ca stringzerotrim
 +ca comdynk
@@ -48214,7 +48237,7 @@ C+ei
       intent (in) element_name, att_name, newValue
       !Functions
       ! temp variables
-      integer el_type, ii
+      integer el_type, ii, j
       character(maxstrlen_dynk) element_name_stripped
       character(maxstrlen_dynk) att_name_stripped
       ! For sanity check
@@ -48237,7 +48260,35 @@ C+ei
       endif
       
 C     Here comes the logic for setting the value of the attribute for all instances of the element...
-      ! Get type
+
+      ! Special non-physical elements
+      if (element_name_stripped .eq. "GLOBAL-VARS") then
+         if (att_name_stripped .eq. "E0" ) then
+            ! Modify the reference particle
+            e0 = newValue
+            e0f = sqrt(e0**2 - pma**2)
+            gammar = pma/e0
+            ! Update particles (see thin6dua)
+            do j = 1, npart
+              dpsv(j) = (ejfv(j) - e0f)/e0f
+              dpsv1(j) = (dpsv(j)*c1e3)/(one + dpsv(j))
+              dpd(j) = one + dpsv(j)
+              dpsq(j) = sqrt(dpd(j))
+              oidpsv(j) = one/(one + dpsv(j))
+              rvv(j) = (ejv(j)*e0f)/(e0*ejfv(j))
+
+!             The update from thin6dua
+!             rvv(j)=(ejv(j)*e0f)/(e0*ejfv(j))
+!             dpsv(j)=(ejfv(j)-e0f)/e0f
+!             oidpsv(j)=one/(one+dpsv(j))
+! !hr01       dpsv1(j)=dpsv(j)*c1e3*oidpsv(j)
+!             dpsv1(j)=(dpsv(j)*c1e3)*oidpsv(j)                            !hr01
+            enddo
+         endif
+         ldoubleElement = .true.
+      endif
+      
+      ! Normal SINGLE ELEMENTs
       do ii=1,il
          ! TODO: Here one could find the right ii in dynk_pretrack,
          ! and then avoid this loop / string-comparison
@@ -48450,6 +48501,16 @@ c$$$            endif
      &    trim(element_name_s)//"', att_name = '"//trim(att_name_s)//"'"
       end if
 
+      ! Special non-physical elements
+      if (element_name_s .eq. "GLOBAL-VARS") then
+         if (att_name_s .eq. "E0" ) then
+            ! Return the energy
+            dynk_getvalue = e0
+         endif
+         ldoubleElement = .true.
+      endif
+      
+      ! Normal SINGLE ELEMENTs
       do ii=1,il
          ! TODO: Here one could find the right ii in dynk_pretrack,
          ! and then avoid this loop / string-comparison
